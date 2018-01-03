db910d88b932188f6a962e903d48ae49a35c7c8d
baguette <cornevin.jolan@gmail.com>
2018-01-02 18:51:23 -0800

    TDD test basic search

--- spec/fuzmoisearch-spec.js
+++ spec/fuzmoisearch-spec.js
@@ -4,4 +4,12 @@ describe("FuzMoiSearch", function() {
     it("Should return empty array by default", function() {
         expect(fuzmoiseach()).toEqual([]);
     });
+    it("Should return result when matching a string in array of string", function() {
+        var list = ['bonjour', 'bonsoir'],
+            search = 'bonjour';
+
+        console.info(fuzmoiseach(list, search));
+
+        expect(fuzmoiseach(list, search)).toEqual([search]);
+    });
 });
