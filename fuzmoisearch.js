ea6df2a6c4376d69c5f3d992eec3e1f45a7d3994
Jolan Cornevin <jcornevin@work4labs.com>
2018-01-03 19:04:07 -0800

    solve test simple search with one result

--- fuzmoisearch.js
+++ fuzmoisearch.js
@@ -5,33 +5,27 @@
 const memoize = require('fast-memoize');
 const fuzzysort = require('fuzzysort');
 
-const options = {
-    shouldSort: true,
-    threshold: 0.6,
-    location: 0,
-    distance: 100,
-    minMatchCharLength: 1
-};
+function _formatResult(results) {
+    var sortedResults = [],
+          result = null,
+          resultsLen = results.length;
 
+    for (var resultIndex = 0; resultIndex < resultsLen; resultIndex ++) {
+        result = results[resultIndex];
+        sortedResults.push(result['target']);
+    }
+
+    return sortedResults;
+}
 
 function fuzmoiseach(list, search, threshold) {
     list = list || [];
     search = search || '';
-    threshold = threshold || -0.5;
+    threshold = threshold || -Infinity;
 
-    var allResults = fuzzysort.go(search, list),
-        sortedResults = [],
-        result = null;
-
-    var resultsLen = allResults.length;
-
-    for (var resultIndex = 0; resultIndex < resultsLen; resultIndex ++) {
-        result = allResults[resultIndex];
-        if (result['score'] > threshold)
-            sortedResults.push(result);
-    }
-
-    return sortedResults;
+    // Do the search
+    var allResults = fuzzysort.go(search, list, {threshold: threshold});
+    return _formatResult(allResults);
 }
 
 
