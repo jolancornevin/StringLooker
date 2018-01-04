3d52381ba9e8d4ab461036eedf16d78612f91de0
Jolan Cornevin <jcornevin@work4labs.com>
2018-01-03 19:03:30 -0800

    TDD test simple search with one result

--- spec/fuzmoisearch-spec.js
+++ spec/fuzmoisearch-spec.js
@@ -1,15 +1,14 @@
 const fuzmoiseach = require('../fuzmoisearch');
 
 describe("FuzMoiSearch", function() {
+
     it("Should return empty array by default", function() {
         expect(fuzmoiseach()).toEqual([]);
     });
+
     it("Should return result when matching a string in array of string", function() {
         var list = ['bonjour', 'bonsoir'],
             search = 'bonjou';
-
-        console.info(fuzmoiseach(list, search));
-
-        expect(fuzmoiseach(list, search)).toEqual([search]);
+        expect(fuzmoiseach(list, search)).toEqual(['bonjour']);
     });
 });
