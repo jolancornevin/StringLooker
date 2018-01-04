6b733fda10c76847bf31cbf50fae0c6a7ce03c83
baguette <cornevin.jolan@gmail.com>
2018-01-03 22:45:40 -0800

    TDD test cache results

--- spec/fuzmoisearch-spec.js
+++ spec/fuzmoisearch-spec.js
@@ -7,10 +7,25 @@ describe("FuzMoiSearch", function() {
     });
 
     it("Should return result when matching a string in array of string", function() {
-        var list = ['bonjour', 'bonsoir'], search = 'bonjou';
-        expect(new FuzzySearch(list, search).search()).toEqual(['bonjour']);
+        var list = ['bonjour', 'bonsoir'],
+            search = 'bonjou';
+        expect(new FuzzySearch(list).search(search)).toEqual(['bonjour']);
 
         search = 'bon';
-        expect(new FuzzySearch(list, search).search()).toEqual(['bonjour', 'bonsoir']);
+        expect(new FuzzySearch(list).search(search)).toEqual(['bonjour', 'bonsoir']);
+    });
+
+    it("Should use cache when redo request", function() {
+        let list = ['bonjour', 'bonsoir'],
+            search = 'bonjou',
+            fuzzy = new FuzzySearch(list),
+            spy = spyOn(FuzzySearch, '_formatResult').and.callThrough();
+
+        expect(fuzzy.search(search)).toEqual(['bonjour']);
+        expect(spy).toHaveBeenCalled();
+        spy.calls.reset();
+
+        expect(fuzzy.search(search)).toEqual(['bonjour']);
+        expect(spy).not.toHaveBeenCalled();
     });
 });
