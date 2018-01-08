08173ad9c2b965b72314082b494db0305d236327
baguette <cornevin.jolan@gmail.com>
2018-01-07 20:49:41 -0800

    TDD test remove fn

--- spec/fuzmoisearch-spec.js
+++ spec/fuzmoisearch-spec.js
@@ -41,13 +41,38 @@ describe("FuzMoiSearch", function () {
         expect(spy).toHaveBeenCalled();
         spy.calls.reset();
 
-        fuzzy.add('bonojur');
-        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonojur']);
+        fuzzy.add('bonjoureuh');
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjoureuh']);
         expect(spy).not.toHaveBeenCalled();
         spy.calls.reset();
 
-        fuzzy.add('bonjour');
-        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjour', 'bonojur']);
+        fuzzy.add('bonjoureuheuh');
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjoureuh', 'bonjoureuheuh']);
         expect(spy).not.toHaveBeenCalled();
-    })
+
+        fuzzy.add('bonjoureuhe');
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjoureuh', 'bonjoureuhe', 'bonjoureuheuh']);
+        expect(spy).not.toHaveBeenCalled();
+    });
+
+    it("Should remove element to list", function () {
+        let list = ['bonjour', 'bonjoureuh', 'bonjoureuhe', 'bonjoureuheuh', 'bonsoir'],
+            search = 'bonjou',
+            fuzzy = new FuzzySearch(list),
+            // We spy on format result because we always use it when doing a request and it's easy to spy
+            spy = spyOn(FuzzySearch, '_formatResult').and.callThrough();
+
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjoureuh', 'bonjoureuhe', 'bonjoureuheuh']);
+        expect(spy).toHaveBeenCalled();
+        spy.calls.reset();
+
+        fuzzy.remove('bonjoureuh');
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjoureuhe', 'bonjoureuheuh']);
+        expect(spy).not.toHaveBeenCalled();
+        spy.calls.reset();
+
+        fuzzy.remove('bonjoureuheuh');
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjoureuhe']);
+        expect(spy).not.toHaveBeenCalled();
+    });
 });
