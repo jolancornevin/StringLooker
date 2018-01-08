a4992afebcafa9c0a1767561bf1012d00f9bb72e
baguette <cornevin.jolan@gmail.com>
2018-01-07 17:26:12 -0800

    TTD test add element

--- spec/fuzmoisearch-spec.js
+++ spec/fuzmoisearch-spec.js
@@ -1,12 +1,12 @@
 import FuzzySearch from '../src/fuzmoisearch';
 
-describe("FuzMoiSearch", function() {
-    it("Should return empty array by default", function() {
+describe("FuzMoiSearch", function () {
+    it("Should return empty array by default", function () {
         let fuzzy = new FuzzySearch();
         expect(fuzzy.search()).toEqual([]);
     });
 
-    it("Should return result when matching a string in array of string", function() {
+    it("Should return result when matching a string in array of string", function () {
         var list = ['bonjour', 'bonsoir'],
             search = 'bonjou';
         expect(new FuzzySearch(list).search(search)).toEqual(['bonjour']);
@@ -15,10 +15,11 @@ describe("FuzMoiSearch", function() {
         expect(new FuzzySearch(list).search(search)).toEqual(['bonjour', 'bonsoir']);
     });
 
-    it("Should use cache when redo request", function() {
+    it("Should use cache on duplicate request", function () {
         let list = ['bonjour', 'bonsoir'],
             search = 'bonjou',
             fuzzy = new FuzzySearch(list),
+            // We spy on format result because we always use it when doing a request and it's easy to spy
             spy = spyOn(FuzzySearch, '_formatResult').and.callThrough();
 
         expect(fuzzy.search(search)).toEqual(['bonjour']);
@@ -28,4 +29,25 @@ describe("FuzMoiSearch", function() {
         expect(fuzzy.search(search)).toEqual(['bonjour']);
         expect(spy).not.toHaveBeenCalled();
     });
+
+    it("Should add element to list", function () {
+        let list = ['bonjour', 'bonsoir'],
+            search = 'bonjou',
+            fuzzy = new FuzzySearch(list),
+            // We spy on format result because we always use it when doing a request and it's easy to spy
+            spy = spyOn(FuzzySearch, '_formatResult').and.callThrough();
+
+        expect(fuzzy.search(search)).toEqual(['bonjour']);
+        expect(spy).toHaveBeenCalled();
+        spy.calls.reset();
+
+        fuzzy.add('bonojur');
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonojur']);
+        expect(spy).not.toHaveBeenCalled();
+        spy.calls.reset();
+
+        fuzzy.add('bonjour');
+        expect(fuzzy.search(search)).toEqual(['bonjour', 'bonjour', 'bonojur']);
+        expect(spy).not.toHaveBeenCalled();
+    })
 });
