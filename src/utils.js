b6f46a6279cc7b26399f716a38e2edabbedeb38e
Jolan Cornevin <jcornevin@work4labs.com>
2018-01-19 19:02:52 -0800

    add quicksort algorithm to utils fn

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const _xOrThreshold = (x, threshold) => {
+const xOrThreshold = (x, threshold) => {
     if (x != null && x != undefined)
         return x;
     return threshold;
@@ -13,7 +13,7 @@ const _xOrThreshold = (x, threshold) => {
  * @returns {*}
  * @private
  */
-const _insertSort = (element, array) => {
+const insertSort = (element, array) => {
     let index = 0,
         arrayLen = array.length;
 
@@ -26,4 +26,41 @@ const _insertSort = (element, array) => {
     };
 };
 
-export {_xOrThreshold, _insertSort}
+const quickSort = (arr) => {return _quickSort(arr, 0, arr.length - 1)}
+const _quickSort = (arr, left, right) => {
+    var len = arr.length,
+        pivot,
+        partitionIndex;
+
+    if (left < right) {
+        pivot = right;
+        partitionIndex = partition(arr, pivot, left, right);
+
+        //sort left and right
+        _quickSort(arr, left, partitionIndex - 1);
+        _quickSort(arr, partitionIndex + 1, right);
+    }
+    return arr;
+};
+
+const partition = (arr, pivot, left, right) => {
+    var pivotValue = arr[pivot].score,
+        partitionIndex = left;
+
+    for(var i = left; i < right; i++) {
+        if(arr[i].score > pivotValue) {
+            swap(arr, i, partitionIndex);
+            partitionIndex++;
+        }
+    }
+    swap(arr, right, partitionIndex);
+    return partitionIndex;
+};
+
+const swap = (arr, i, j) => {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+};
+
+export {xOrThreshold, insertSort, quickSort}
